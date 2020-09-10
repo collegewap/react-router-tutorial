952c234b5fdf17fd8bead3c5b872abc297a6a0db
Abhishek EH <collegewap@gmail.com>
2020-09-10 17:59:57 +0530

    Basic Routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="about" element={<About />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
+
+export const Home = () => {
+  return <div>You are in Home page</div>;
+};
+export const About = () => {
+  return <div>This is the page where you put details about yourself</div>;
+};
+export const NotFound = () => {
+  return <div>This is a 404 page</div>;
+};
 
 export default App;
