bc0cb3ebb61d65ac0e2c4fd17d3d524d4e62b7ba
Abhishek EH <collegewap@gmail.com>
2020-09-11 13:02:47 +0530

    URL parameter

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Link, Route } from "react-router-dom";
+import { Routes, Link, Route, useParams } from "react-router-dom";
 
 const Dashboard = () => {
   return (
@@ -20,6 +20,7 @@ const Dashboard = () => {
           <Route path="/" element={<Profile />}></Route>
           <Route path="orders" element={<Orders />}></Route>
           <Route path="quotes" element={<Quotes />}></Route>
+          <Route path="order_details/:orderId" element={<OrderDetails />} />
         </Routes>
       </div>
     </div>
@@ -30,10 +31,32 @@ export const Profile = () => {
   return <h2>Profile</h2>;
 };
 export const Orders = () => {
-  return <h2>Orders</h2>;
+  const orderIds = ["10001", "10002", "10003"];
+  return (
+    <>
+      <h2>Orders</h2>
+      <ul className="orders">
+        {/* Loop through the orders array and display link to order details */}
+        {orderIds.map((orderId) => {
+          return (
+            <li key={orderId}>
+              <Link to={`/dashboard/order_details/${orderId}`}>
+                View Order {orderId}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 export const Quotes = () => {
   return <h2>Quotes</h2>;
+};
+export const OrderDetails = () => {
+  const params = useParams();
+
+  return <h2>Details of order {params.orderId}</h2>;
 };
 
 export default Dashboard;
