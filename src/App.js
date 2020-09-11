43065cf7c5e02aa5f19d54572f42a71aa0147c48
Abhishek EH <collegewap@gmail.com>
2020-09-11 10:13:30 +0530

    Nested Routes

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="about" element={<About />}></Route>
-          <Route path="dashboard" element={<Dashboard />}></Route>
+          <Route path="dashboard/*" element={<Dashboard />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </div>
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Routes, Link, Route } from "react-router-dom";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="">Profile</Link>
+        </li>
+        <li>
+          <Link to="orders">Orders</Link>
+        </li>
+        <li>
+          <Link to="quotes">Quotes</Link>
+        </li>
+      </ul>
+      <div className="dashboard">
+        <Routes>
+          <Route path="/" element={<Profile />}></Route>
+          <Route path="orders" element={<Orders />}></Route>
+          <Route path="quotes" element={<Quotes />}></Route>
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export const Profile = () => {
+  return <h2>Profile</h2>;
+};
+export const Orders = () => {
+  return <h2>Orders</h2>;
+};
+export const Quotes = () => {
+  return <h2>Quotes</h2>;
 };
 
 export default Dashboard;
